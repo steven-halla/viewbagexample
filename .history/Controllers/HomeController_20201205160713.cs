3315707488848c758bcfd844df121707af151732
steven-halla <69405488+steven-halla@users.noreply.github.com>
2020-12-05 16:13:20 -0800

    first commit

---
+++ .history/Controllers/HomeController_20201205160713.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dojosurvey.Models;
+
+namespace dojosurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Http]
+    }
+}
---
+++ .history/Controllers/HomeController_20201205160735.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dojosurvey.Models;
+
+namespace dojosurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            
+        }
+    }
+}
